56a2b125deaed3c4c20898ffc1eee7a1b466099b
Yatoshk <chesalova2013@gmail.com>
2022-07-29 12:04:51 +0400

    DB

---
+++ src/main/java/DataSource/ApplDataSource.java
@@ -0,0 +1,32 @@
+package DataSource;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class ApplDataSource {
+    private static final PGSimpleDataSource ds;
+    private static final Connection connection;
+
+    static {
+        ds = new PGSimpleDataSource();
+
+        ds.setServerNames(new String[]{"localhost:5432"});
+        ds.setDatabaseName("players");
+        ds.setUser("postgres");
+        ds.setPassword("root");
+
+        try {
+
+            connection = ds.getConnection();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
