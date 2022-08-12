00ee452e4309c56363698c0df963f085f2c3f3e7
Yatoshk <chesalova2013@gmail.com>
2022-08-12 20:17:36 +0400

    done

--- src/main/java/repository/Game.java
+++ src/main/java/repository/Game.java
@@ -11,6 +11,8 @@ public class Game {
 
     public char[][] playingField = new char[3][3];
     public ArrayList<String> availableMoves = new ArrayList<>();
+    public ArrayList<String> availableMovesCopy = new ArrayList<>();
+    public ArrayList<String> madeMoves = new ArrayList<>();
     Data d = new Data();
     RegistrationPlayer pl = new RegistrationPlayer();
     Player player1 = pl.loginVerification();
@@ -28,9 +30,9 @@ public class Game {
         for (char i = 'a'; i <= 'c'; i++){
             for (int j = 1; j <= 3; j++) {
                 availableMoves.add(i + ""+ j + "");
+                availableMovesCopy.add(i + ""+ j + "");
             }
         }
-
         int count = 0;
 
         System.out.println("         ");
@@ -47,23 +49,59 @@ public class Game {
         player2.view(player2);
 
         boolean contGame = false;
-
-        if (findOldGame(player1.getFio(), player2.getFio(), listOldGames)){
+        int indx = findOldGame(player1.getFio(), player2.getFio(), listOldGames);
+        if (indx != -1){
             System.out.println("Do you want to continue the old game? (y/n)");
             Scanner ans = new Scanner(System.in);
             String answer = ans.nextLine();
-
-            //todo при продолжении игры ошибки: 1. нужно вывести все шаги которые уже использовались + доступные 2. выбираются использованные шаги
-
-
             if (answer.equals("y")){
                 contGame = true;
-                for (var v: availableMoves){
-                    if(listOldGames.contains(v)){
+                for (var v: availableMovesCopy){
+                    if (listOldGames.get(indx).moves.contains(v)) {
                         count++;
+                        madeMoves.add(v);
                         availableMoves.remove(v);
                     }
                 }
+                char k = ' ';
+                if (Objects.equals(listOldGames.get(indx).sign1, "x")){
+                    k = 'x';
+                    System.out.println("Turn player 1 == x, player 2 == o. ");
+                }
+                else if (Objects.equals(listOldGames.get(indx).sign1, "o")){
+                    k = 'o';
+                    System.out.println("Turn player 1 == o, player 2 == x. ");
+                }
+                for(var v: madeMoves){
+                    switch (v) {
+                        case "a1" -> playingField[0][0] = k;
+                        case "a2" -> playingField[0][1] = k;
+                        case "a3" -> playingField[0][2] = k;
+                        case "b1" -> playingField[1][0] = k;
+                        case "b2" -> playingField[1][1] = k;
+                        case "b3" -> playingField[1][2] = k;
+                        case "c1" -> playingField[2][0] = k;
+                        case "c2" -> playingField[2][1] = k;
+                        case "c3" -> playingField[2][2] = k;
+                    }
+                    if (k == 'x')
+                        k = 'o';
+                    else
+                        k = 'x';
+                }
+                System.out.println(" ");
+                System.out.println(" " + playingField[0][0] + "  | " + playingField[0][1] + " |  " + playingField[0][2] + " ");
+                System.out.println(" -----------");
+                System.out.println(" " + playingField[1][0] + "  | " + playingField[1][1] + " |  " + playingField[1][2] + " ");
+                System.out.println(" -----------");
+                System.out.println(" " + playingField[2][0] + "  | " + playingField[2][1] + " |  " + playingField[2][2] + " ");
+                //
+                System.out.println("You make moves: ");
+                for (var v: availableMoves){
+                    System.out.print(v + " ");
+                }
+                System.out.println("");
+
             }
             else if (answer.equals("n"))
                 d.deleteOldGame(player1.getFio(), player2.getFio());
@@ -95,24 +133,25 @@ public class Game {
             System.out.println(" ---------------");
             System.out.println(" c1  | c2 |  c3");
         }
-        boolean check;
-        int flag;
+        boolean check = true;
+        int flag = 0;
         String m1, m2;
         Scanner s1 = new Scanner(System.in);
         Scanner s2 = new Scanner(System.in);
         do{
-            System.out.println("choice of moves pl1:");
-
-            m1 = s1.nextLine();
-            while (!checkAvailableMoves(m1, availableMoves)){
-                System.out.println("the move has already been made, choose another:");
+            if(count % 2 == 0 && check){
+                System.out.println("choice of moves pl1:");
                 m1 = s1.nextLine();
+                while (!checkAvailableMoves(m1, availableMoves)){
+                    System.out.println("the move has already been made, choose another:");
+                    m1 = s1.nextLine();
+                }
+                player1.move = m1;
+                count++;
+                check = checkingMoves(player1, playingField, count);
+                flag = 1;
             }
-            player1.move = m1;
-            count++;
-            check = checkingMoves(player1, playingField, count);
-            flag = 1;
-            if (check){
+            if (count % 2 != 0 && check){
                 System.out.println("choice of moves pl2:");
                 m2 = s2.nextLine();
                 while (!checkAvailableMoves(m2, availableMoves)){
@@ -140,7 +179,6 @@ public class Game {
             }
         }
         d.deleteOldGame(player1.getFio(), player2.getFio());
-
         System.out.println("Update data all players: ");
         pl.viewLiders("");
     }
@@ -166,15 +204,12 @@ public class Game {
             case "c3" -> playingField[2][2] = t;
         }
 
-        //вывод ходов
-
         System.out.println(" " + playingField[0][0] + "  | " + playingField[0][1] + " |  " + playingField[0][2] + " ");
         System.out.println(" -----------");
         System.out.println(" " + playingField[1][0] + "  | " + playingField[1][1] + " |  " + playingField[1][2] + " ");
         System.out.println(" -----------");
         System.out.println(" " + playingField[2][0] + "  | " + playingField[2][1] + " |  " + playingField[2][2] + " ");
 
-        //проверка на ходы todo улучшить
         // ---
         if (playingField[0][0] != ' ' && playingField[0][0] == playingField[0][1] && playingField[0][1] == playingField[0][2])
         {
@@ -233,15 +268,17 @@ public class Game {
         return false;
     }
 
-    private boolean findOldGame(String fio1, String fio2, List<SaveGame> list){
+    private int findOldGame(String fio1, String fio2, List<SaveGame> list){
+        int count = 0;
         for (var v: list){
             if (fio1.equals(v.fio1) && fio2.equals(v.fio2))
             {
                 player1.sign = v.sign1;
                 player2.sign = v.sign2;
-                return true;
+                return count;
             }
+            count++;
         }
-        return false;
+        return -1;
     }
 }
--- src/main/java/repository/Main.java
+++ src/main/java/repository/Main.java
@@ -1,10 +1,12 @@
 package repository;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         Game g = new Game();
         g.startGame();
-
     }
 }
 
